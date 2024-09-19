567e26968bffd2986f0edd2e7e74335c5aa95a4f
venson <venson.hunt@gmail.com>
2024-09-19 20:34:53 +0800

    add harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,47 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+
+      harpoon:setup()
+
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<C-t>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', '<C-a>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-i>', function()
+        harpoon:list():select(4)
+      end)
+      vim.keymap.set('n', '<leader><C-a>', function()
+        harpoon:list():replace_at(1)
+      end)
+      vim.keymap.set('n', '<leader><C-e>', function()
+        harpoon:list():replace_at(2)
+      end)
+      vim.keymap.set('n', '<leader><C-h>', function()
+        harpoon:list():replace_at(3)
+      end)
+      vim.keymap.set('n', '<leader><C-i>', function()
+        harpoon:list():replace_at(4)
+      end)
+    end,
+  },
+}
